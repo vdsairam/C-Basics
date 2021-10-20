6b71144144a69ee3dfc75b853f3e84d67e930944
vdsairam <92287971+vdsairam@users.noreply.github.com>
2021-10-20 10:57:24 +0530

    Add files via upload

---
+++ Float division program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaratio
+	float n1, n2, division;
+	
+	//initialization
+	n1 = 15.3;
+	n2 = 20.2;
+	
+	//calculation
+	division = n1/n2;
+	
+	//output
+	printf("Division of %f and %f is %f", n1, n2, division);
+}
