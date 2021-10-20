4a7689e7e4d70a89d99b3caccbfb82f6e1990c68
vdsairam <92287971+vdsairam@users.noreply.github.com>
2021-10-20 10:41:04 +0530

    Add files via upload

---
+++ adding two numbers.c
@@ -0,0 +1,17 @@
+/* Adding two numbers */
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int number1, number2, sum;
+	
+	//initialisation
+	number1 = 52;
+	number2 = 38;
+	
+	//calculation
+	sum = number1+number2;
+	
+	//output
+	printf("sum of %d and %d is %d", number1, number2, sum );
+}
---
+++ data type program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialisation
+	age = 19;
+	cgpa = 7.01;
+	initial = 'S';
+	
+	//output
+	printf("Age of the student is %d\n",age);
+	printf("CGPA of the student is %f\n",cgpa);
+	printf("Initial of the student is %c\n",initial);
+}
