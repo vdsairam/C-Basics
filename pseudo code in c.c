2fe76dedc0ee35e363661d6897e34c6530ab1fcb
vdsairam <92287971+vdsairam@users.noreply.github.com>
2021-12-14 10:35:28 +0530

    Add files via upload

---
+++ pseudo code in c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int p,q,r;
+	scanf("%d%d%d",&p,&q,&r);
+	p=(6+5)&p;
+	if((q-3)<(3+q))
+	{
+	q=q+p;
+	if((7^r^q)<(4+q+p))
+	r=(12&4)&r;
+	r=10&r;
+    }
+	{
+	printf("%d",p+q+r);
+    }
+}
